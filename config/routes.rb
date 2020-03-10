1ed0f727945ba5c2c77f6ff910a0779ed83cc87e
takahiro-ueda <pt25ue@gmail.com>
2020-03-10 16:12:40 +0900

    変動費科目機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   resources :incomes
   resources :fixedcosts
+  resources :variablecosts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
