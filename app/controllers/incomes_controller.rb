af8cccfa71dd5a43c81677c29f0bf550f8e2f91e
takahiro-ueda <pt25ue@gmail.com>
2020-03-10 14:17:27 +0900

    収入科目設定機能

---
+++ app/controllers/incomes_controller.rb
@@ -0,0 +1,43 @@
+class IncomesController < ApplicationController
+  def index
+    @incomes = Income.order(created_at: :asc)
+  end
+
+  def show
+    @income = Income.find(params[:id])
+  end
+
+  def new
+    @income = Income.new()
+  end
+
+  def edit
+    @income = Income.find(params[:id])
+  end
+
+  def create
+    @income = Income.new(params[:income])
+    if @income.save
+      redirect_to @income, notice: "収入科目を登録する"
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @income = Income.find(params[:id])
+    @income.assign_attributes(params[:income])
+    if @income.save
+      redirect_to @income, notice: "収入科目を保存しました"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @income = Income.find(params[:id])
+    @income.destroy
+    redirect_to :incomes, notice: "科目を削除しました"
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'top/index'
+  root "top#index"
+
+  resources :incomes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
