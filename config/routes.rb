9dddcc513a39ffbe4e9f482391df9613b338e48d
takahiro-ueda <pt25ue@gmail.com>
2020-03-10 15:21:04 +0900

    固定費科目機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root "top#index"
 
   resources :incomes
+  resources :fixedcosts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
