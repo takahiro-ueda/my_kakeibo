b9ef21d9c2a95e7e393c667c2d996b0f112b50a5
takahiro-ueda <pt25ue@gmail.com>
2020-03-11 12:22:54 +0900

    固定費科目データ登録

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # get 'top/index'
   root "top#index"
   post "income_values/new(/:name)" => "income_values#new"
+  post "fixedcost_values/new(/:name)" => "fixedcost_values#new"
 
   resources :incomes
   resources :fixedcosts
   resources :variablecosts
   resources :income_values
+  resources :fixedcost_values
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
