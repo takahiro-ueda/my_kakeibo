8422799dfe68841d0e5f6f0e76ba08286861b714
takahiro-ueda <pt25ue@gmail.com>
2020-03-11 14:52:35 +0900

    変動費科目データ登録

---
+++ app/models/variablecost_value.rb
@@ -0,0 +1,2 @@
+class VariablecostValue < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   root "top#index"
   post "income_values/new(/:name)" => "income_values#new"
   post "fixedcost_values/new(/:name)" => "fixedcost_values#new"
+  post "variablecost_values/new(/:name)" => "variablecost_values#new"
 
   resources :incomes
   resources :fixedcosts
   resources :variablecosts
   resources :income_values
   resources :fixedcost_values
+  resources :variablecost_values
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
